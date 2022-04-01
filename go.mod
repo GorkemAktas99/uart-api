186c9020a961d607ee29742a1ffaacb3a5c65da4
GorkemAktas99 <72526481+GorkemAktas99@users.noreply.github.com>
2022-04-01 01:16:17 +0000

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,8 @@
+module backendvm
+
+go 1.17
+
+require (
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07 // indirect
+	golang.org/x/sys v0.0.0-20220330033206-e17cdc41300f // indirect
+)
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/tarm/serial"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type payload struct {
+	Workspace string `json:"workspace"`
+	ComPort   string `json:"comPort"`
+	Cmd       string `json:"cmd"`
+	Timer     int    `json:"timer"`
+}
+
+type outPayload struct {
+	Workspace string `json:"workspace"`
+	ComPort   string `json:"comPort"`
+	Cmd       string `json:"cmd"`
+	Output    string `json:"output"`
+}
+
+var newPayload payload
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	enableCors(w)
+	json.NewDecoder(r.Body).Decode(&newPayload)
+	if newPayload.Cmd != "" {
+		log.Println("Command is running on the board...")
+		output := runCommand()
+		log.Println("Command Run has been completed")
+		response := outPayload{
+			Workspace: newPayload.Workspace,
+			ComPort:   newPayload.ComPort,
+			Cmd:       newPayload.Cmd,
+			Output:    output,
+		}
+		fmt.Println(response.Output)
+		log.Println("Command Script has been changed to default")
+		json.NewEncoder(w).Encode(response)
+	} else {
+		response := outPayload{
+			Workspace: newPayload.Workspace,
+			ComPort:   newPayload.ComPort,
+			Cmd:       newPayload.Cmd,
+			Output:    "You has been sent empty command",
+		}
+		json.NewEncoder(w).Encode(response)
+	}
+
+}
+func main() {
+	log.Println("Program has been stated")
+	log.Println("Backend is running on 5000 Port")
+	http.HandleFunc("/exec", handler)
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
+
+func runCommand() (outPutBoard string) {
+	c := &serial.Config{Name: newPayload.ComPort, Baud: 115200}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := []byte(newPayload.Cmd + "\r")
+	fmt.Println(data)
+	n, err := s.Write(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	time.Sleep(time.Millisecond * 300)
+	buf := make([]byte, 128)
+	n, err = s.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	senderData := string(buf[:n])
+	sender := strings.Split(senderData, "\n")
+	s.Close()
+	return sender[1]
+}
+
+func enableCors(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+}
